af7556b105e82dcb62937be59f8215302a69485c
François Schwarzentruber <francois.schwarzentruber@ens-lyon.fr>
2026-09-09 15:20:55 +0200

    Makefile

--- app.py
+++ app.py
@@ -285,4 +285,8 @@ def resize():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(
+        host=os.environ.get("HOST", "127.0.0.1"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=os.environ.get("FLASK_DEBUG", "1") == "1",
+    )
